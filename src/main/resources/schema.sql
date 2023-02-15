54b0e9b64e2d99474731e1195e27af6b70a40b0e
Dan Vega <danvega@gmail.com>
2023-02-15 13:42:17 -0500

    Updating for railway!

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,10 @@
 CREATE TABLE IF NOT EXISTS Content (
-    id INTEGER AUTO_INCREMENT,
+    id SERIAL PRIMARY KEY ,
     title varchar(255) NOT NULL,
-    desc text,
+    description text,
     status VARCHAR(20) NOT NULL,
     content_type VARCHAR(50) NOT NULL,
     date_created TIMESTAMP NOT NULL,
     date_updated TIMESTAMP,
-    url VARCHAR(255),
-    primary key (id)
+    url VARCHAR(255)
 );
